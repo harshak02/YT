4110d810064ca31285eb3625e6ad5b2f5ee5973e
harshak02 <shkancharla7777@gmail.com>
2023-04-22 16:39:36 +0530

    push

---
+++ server/routes/videos.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { addVideo, addView, deleteVideo, getByTag, getVideo, random, search, sub, trend, updateVideo } from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
+
+const router = express.Router();
+
+router.post("/",verifyToken,addVideo);
+
+router.put("/:id",verifyToken,updateVideo);
+
+router.delete("/:id",verifyToken,deleteVideo);
+
+router.get("/find/:id",getVideo);
+
+router.put("/view/:id",addView);//to update the views while watching a video
+//verify token is used to verify the current user with the operation he needed to be performed
+//look carefully to increase views if we use the verify token then it means only he can add the views of his
+//only videos uploaded if we use (req.user.id !== video.userId) else if we we wanted to increase views only if
+//he is logged in then dont use that line thats it... (here we are not considereing anything even if he is logged
+//in or not we increase the views functionality)
+
+router.get("/trend",trend);
+
+router.get("/random",random);
+
+router.post("/sub",verifyToken,sub);
+
+router.get("/tags",getByTag);
+
+router.get("/search",search);
+
+export default router;
---
+++ server/verifyToken.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { createError } from "./error.js";
+
+export const verifyToken = async (req,res,next) => {
+    // const token = req.cookies.access_token
+    const token = req.body.token //sending manually
+    if(!token){
+        return next(createError(401,"Unauthenticated User!"));
+    }
+    jwt.verify(token,process.env.JWT,(err,user)=>{
+        if(err){
+            return next(createError(403,"Token is not valid!"));
+        }
+        req.user = user; //assigning user id for further applications
+        next(); //continues from where we left 
+    })
+}
