ddc8b074a70f4860e96b3507d71f07b8a451dd12
harshak02 <shkancharla7777@gmail.com>
2023-05-01 19:50:35 +0530

    Completed only responsive part is left

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Comment from "./Comment";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import SendIcon from '@mui/icons-material/Send';
 
 const Container = styled.div`
   padding : 20px 0px;
@@ -30,11 +31,16 @@ const Input = styled.input`
   width: 100%;
 `;
 
+const Button = styled.div`
+  color : ${({ theme }) => theme.text};
+`;
+
 //we no need to use currentVideo._id because aldready in Video we have sent the props of video_id
 export const Comments = ({videoId}) => {
 
   const [comments , setComments] = useState([]);
   const { currentUser } = useSelector((state)=>state.user);
+  const { currentVideo } = useSelector((state)=>state.video);
 
   useEffect(() => {
     const fetchComments = async () => {
@@ -48,11 +54,23 @@ export const Comments = ({videoId}) => {
     fetchComments();
   })
 
+  const [commentDesc,setCommentDesc] = useState("");
+
+  //here we are getting directlt because we are using the useState as in page we are doing things
+  const handleComment = async () => {
+    const res = await axios.post("http://localhost:8800/api/comments/",{
+      videoId : currentVideo._id,
+      desc : commentDesc,
+      token : currentUser.token,//req.user.id is taken from the verify token
+    });
+  }
+
   return (
     <Container>
       <NewComment>
         <Avatar src={currentUser ? currentUser.otherDetails.img : "abcd"} />
-        <Input placeholder="Add a comment..." />
+        <Input placeholder="Add a comment..." onChange={e=>setCommentDesc(e.target.value)}/>
+        <Button onClick={handleComment}><SendIcon /></Button>
       </NewComment>
       {comments.map (comment => (
         <Comment key={comment._id} comment = {comment} />
